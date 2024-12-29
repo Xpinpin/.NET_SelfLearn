3f768f29a4163ba2255bd199d3e334ef40721dc0
Xpinpin <55543578+Xpinpin@users.noreply.github.com>
2024-12-29 18:20:28 -0400

    Bubble Sort
    Sap Xep Noi Bot (Bubble Sort)

---
+++ TimKiemTrongMang/SapXep/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.Metrics;
+
+var rand = new Random();
+int k;
+var arr = new int[10];
+
+for (k = 0; k < arr.Length; k++)
+{
+    arr[k] = rand.Next() % 1000;
+}
+
+foreach (var n in arr)
+{
+    Console.Write($"{n}\t");
+}
+
+//sap xep va in ra
+for (int i = 0; i < arr.Length - 1; i++)
+{
+    for (int j = arr.Length - 1; j > i; j--)
+    {
+        if (arr[i] > arr[j])
+        {
+            int m = arr[i];
+            arr[i] = arr[j];
+            arr[j] = m;
+        }
+    }
+}
+
+Console.WriteLine("\n--------------------------------\nDa sap xep: "); 
+foreach (var n in arr)
+{
+    Console.Write($"{n}\t");
+}
---
+++ TimKiemTrongMang/TimKiemTrongMang/Program.cs
@@ -0,0 +1,33 @@
+﻿var rand = new Random();
+int i;
+var arr = new int[10];
+
+for(i = 0; i < arr.Length; i++)
+{
+    arr[i] = rand.Next() % 1000;
+}
+
+foreach (var n in arr)
+{
+    Console.Write($"{n}\t");
+}
+
+Console.WriteLine("\nNhap so can tim: ");
+var s = Console.ReadLine();
+int soCanTim = int.Parse(s);
+
+i = 0;
+while (i < arr.Length && soCanTim != arr[i])
+{
+    i++;
+}
+
+if (i == arr.Length)
+{
+    Console.WriteLine("Ko tim thay.");
+
+}
+else
+{
+    Console.WriteLine($"{soCanTim} duoc tim thay tai vi tri {i}");
+}
